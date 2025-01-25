b1aca27483609f50746669bb11531c26601e5742
kushwahavipin <kushwahavipin786@gmail.com>
2025-01-25 10:44:41 +0530

    Index.js Update Project Routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router_user from './routes/user.routes.js';
 import router_reports from './routes/reports.routes.js';
+import projectRoutes from "./routes/projectRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/user",router_user)
 app.use("/api/reports",router_reports)
+app.use("/api/projects", projectRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
