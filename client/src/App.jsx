11b1f8e5a5ff2c9ac833cdbe3fe6970fe1e49e62
kushwahavipin <kushwahavipin786@gmail.com>
2025-01-25 16:03:25 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
 import RecommendedTeammates from './components/RecommendedTeammates';
+import UserProfile from './components/userProfile'
 
 function App() {
   const [searchQuery, setSearchQuery] = useState(''); 
