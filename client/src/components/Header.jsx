94b44fa6be05b0573d9e7f8203ace5f5f38785a0
Tinu Prajapati <tinup2580@gmail.com>
2025-01-25 20:16:34 +0530

    change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,68 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bell } from 'lucide-react';
-import {NavLink,Link} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
-    return (
-        <header className="bg-white shadow-sm sticky top-0 z-10 h-[10vh] w-full flex items-center justify-between px-6 py-4">
-            <Link to="/" className="flex shrink-0 items-center">
-                <img className="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-            </Link>
-            <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                    <a className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Squads</a>
-                    <a className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Find a Member</a>
-                    <a className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Projects</a>
-                </div>
-            </div>
+    const [scrolled, setScrolled] = useState(false);
 
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0" onMouseLeave={() => setOpen(false)}>
-                <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
-                    <span className="sr-only">View notifications</span>
-                    <Bell className="size-6" />
-                </button>
-                <div className="relative ml-3" >
-                    <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none" onClick={() => setOpen(true)}>
-                        <img className="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="User" />
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <header className={`sticky ${scrolled ? "top-[-100%]" : "top-2"} z-10 h-[10vh] w-full flex items-center justify-center transition-all duration-300`}>
+            <div className='w-[90%] h-full bg-white/80 backdrop-blur-xl flex items-center justify-between px-3 rounded-full shadow-lg'>
+                <Link to="/" className="flex shrink-0 items-center">
+                    <img className="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                </Link>
+                <div className="hidden sm:block">
+                    <div className="flex space-x-4">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => `${isActive ? "bg-gray-900 text-white" : "hover:text-white hover:bg-gray-700"} rounded-md  px-3 py-2 text-sm font-medium `} aria-current="page"
+                        >
+                            Squads
+                        </NavLink>
+                        <NavLink
+                            to="/members"
+                            className={({ isActive }) => `${isActive ? "bg-gray-900 text-white" : "hover:text-white hover:bg-gray-700"} rounded-md  px-3 py-2 text-sm font-medium `} aria-current="page"
+                        >
+                            Find a Member
+                        </NavLink>
+                        <NavLink
+                            to="/projects"
+                            className={({ isActive }) => `${isActive ? "bg-gray-900 text-white" : "hover:text-white hover:bg-gray-700"} rounded-md  px-3 py-2 text-sm font-medium `} aria-current="page"
+                        >
+                            Projects
+                        </NavLink>
+                    </div>
+                </div>
+
+                <div className="relative flex items-center space-x-4 sm:ml-6">
+                    <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
+                        <span className="sr-only">View notifications</span>
+                        <Bell className="size-6" />
                     </button>
-                    <div className={`${open ? "block" : "hidden"} absolute right-0 z-10 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none`}>
-                        <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700">Your Profile</Link>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700">Settings</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700">Sign out</a>
+                    <div className="relative">
+                        <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none" onClick={() => setOpen(!open)}>
+                            <img className="size-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="User" />
+                        </button>
+                        {open && (
+                            <div className="absolute right-0 z-10 w-48 mt-2 rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5">
+                                <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Your Profile</Link>
+                                <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</Link>
+                                <Link to="/signout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign out</Link>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
