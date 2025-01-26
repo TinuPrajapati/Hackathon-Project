2b6016b23a4961fd21b3ec9cb93ac7dcff7c0968
kushwahavipin <kushwahavipin786@gmail.com>
2025-01-26 13:50:48 +0530

    MCQ Completed

--- server/routes/mcqRoutes.js
+++ server/routes/mcqRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { generateMCQ } from "../controllers/mcqController.js";
+import { generateMCQ, submitAnswers } from "../controllers/mcqController.js";
 
 const router = express.Router();
 
+// Define routes
 router.post("/generate-mcq", generateMCQ);
+router.post("/submit-answers", submitAnswers);
 
 export default router;
